07fa995ea59e6347b5b9cac23814f9954f72970c
Isaiah186 <91351729+Isaiah186@users.noreply.github.com>
2021-10-22 18:02:44 -0500

    Add files via upload

---
+++ Spiral Helix Turtle Drawing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import turtle
+loadWindow = turtle.Screen()
+turtle.speed(100)
+
+for i in range(100):
+    turtle.circle(5*i)
+    turtle.circle(-5*1)
+    turtle.left(i)
+
+turtle.exitonclick()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
